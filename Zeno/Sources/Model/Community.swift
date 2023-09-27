3b1a7ec56fbcabe028cef6172a1a73717614dc1b
Geonseob Kim <gnksbm@gmail.com>
2023-09-27 13:18:50 +0900

    [Fix] TabBarView의 Tab enum으로 분리

--- Zeno/Sources/Model/Community.swift
+++ Zeno/Sources/Model/Community.swift
@@ -15,8 +15,9 @@ struct Community: Identifiable {
 	/// 커뮤니티 소개
 	var description: String
 
-  var communityImage: String {
-        return "LLLogo" } // 임 시로만들어놨음
+    var communityImage: String {
+        return "LLLogo"
+    } // 임시로 만들어놨음
 	// var communityUserId: [String] // 소속된 유저 아이디, 변수명 변경 필요
 
 	/// 커뮤니티 생성일
@@ -32,6 +33,6 @@ extension Community {
 		.init(communityName: "야곰 아카데미 iOS챌린지 5기", description: "야~~~곰", createdAt: Date().timeIntervalSince1970),
 		.init(communityName: "할맥 모임 88기", description: "마셔마셔 먹고 죽어", createdAt: Date().timeIntervalSince1970),
 	]
-   static let CommunitySamples: [Community] = [Community(communityName: "멋쟁이 사자처럼", description: "세계 최고 부트 캠프 멋쟁이 사자처럼입니다~", createdAt: 20230603), Community(communityName: "새싹 영등포 2기", description: "지구 최고 부트 캠프 새싹 입니다~", createdAt: 20230203), Community(communityName: "야곰 부트캠프 3기", description: "우주 최고 부트캠프 야곰 부트캠프 입니다 ~", createdAt: 20240705)]
+    static let CommunitySamples: [Community] = [Community(communityName: "멋쟁이 사자처럼", description: "세계 최고 부트 캠프 멋쟁이 사자처럼입니다~", createdAt: 20230603), Community(communityName: "새싹 영등포 2기", description: "지구 최고 부트 캠프 새싹 입니다~", createdAt: 20230203), Community(communityName: "야곰 부트캠프 3기", description: "우주 최고 부트캠프 야곰 부트캠프 입니다 ~", createdAt: 20240705)]
 }
 #endif
