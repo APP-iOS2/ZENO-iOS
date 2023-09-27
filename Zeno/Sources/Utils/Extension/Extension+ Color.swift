5daa6d7301890032b14d69cc67ae413d7d610a1f
MUKER-WON <muker.dev@gmail.com>
2023-09-27 14:07:19 +0900

    Update Extension+ Color.swift

--- Zeno/Sources/Utils/Extension/Extension+ Color.swift
+++ Zeno/Sources/Utils/Extension/Extension+ Color.swift
@@ -11,4 +11,22 @@ import SwiftUI
 extension Color {
     static let mainColor = Color(red: 123/255, green: 103/255, blue: 200/255)
     static let ggullungColor = Color(red: 49/255, green: 43/255, blue: 92/255)
+	
+	static func hex(_ hex: String) -> Self {
+		let scanner = Scanner(string: hex)
+		scanner.currentIndex = .init(utf16Offset: 0, in: hex)
+		var rgbValue: UInt64 = 0
+		scanner.scanHexInt64(&rgbValue)
+
+		let r = (rgbValue & 0xff0000) >> 16
+		let g = (rgbValue & 0xff00) >> 8
+		let b = rgbValue & 0xff
+
+		return Self.init(
+			red: Double(r) / 0xff,
+			green: Double(g) / 0xff,
+			blue: Double(b) / 0xff,
+			opacity: 1
+		)
+	}
 }
