f7e375468ea4933ff3f1123eeb59426f8cfacf41
MUKER-WON <muker.dev@gmail.com>
2023-10-01 09:52:57 +0900

    Update ContentViewModel.swift

--- Zeno/Sources/Core/ViewModel/ContentViewModel.swift
+++ Zeno/Sources/Core/ViewModel/ContentViewModel.swift
@@ -19,15 +19,22 @@ class ContentViewModel: ObservableObject {
 	init() {
 		setupSubscribers()
 	}
-	
+	/// 컴바인으로 AuthService의 userSession과 currentUser sink하기
 	func setupSubscribers() {
-		/// Combine으로 싱글톤의 userSession를 해당 viewModel과 sink 맞추기
-		AuthService.shared.$userSession.sink { [weak self] userSession in
-			self?.userSession = userSession
-		}.store(in: &cancellables)
-		/// Combine으로 싱글톤의 currentUser를 해당 viewModel과 sink 맞추기
-		AuthService.shared.$currentUser.sink { [weak self] currentUser in
-			self?.currentUser = currentUser
-		}.store(in: &cancellables)
+		// Combine으로 싱글톤의 userSession를 해당 viewModel과 sink 맞추기
+		AuthService.shared.$userSession
+			.receive(on: DispatchQueue.main)
+			.sink { [weak self] userSession in
+				self?.userSession = userSession
+			}
+			.store(in: &cancellables)
+		
+		// Combine으로 싱글톤의 currentUser를 해당 viewModel과 sink 맞추기
+		AuthService.shared.$currentUser
+			.receive(on: DispatchQueue.main)
+			.sink { [weak self] currentUser in
+				self?.currentUser = currentUser
+			}
+			.store(in: &cancellables)
 	}
 }
