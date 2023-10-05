b7982c5412fdd6f0f184e2725e667c994a178ca9
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2023-10-05 09:43:27 +0900

    [수정] userViewModel 코인, 초성 확인권 사용 업데이트 함수 수정

--- Zeno/Sources/ViewModel/UserViewModel.swift
+++ Zeno/Sources/ViewModel/UserViewModel.swift
@@ -80,27 +80,15 @@ class UserViewModel: ObservableObject {
     func updateUserCoin(to: Int) async {
         guard let currentUser else { return }
         var coin = currentUser.coin
-        coin += to
+        self.currentUser?.coin += to
         try? await FirebaseManager.shared.update(data: currentUser, value: \.coin, to: coin)
-        try? await loadUserData()
     }
     /// 초성확인권 사용 업데이트 함수
     func updateUserInitialCheck(to: Int) async {
         guard let currentUser else { return }
         var initialCheck = currentUser.showInitial
-        initialCheck += to
+        self.currentUser?.showInitial += to
         try? await FirebaseManager.shared.update(data: currentUser, value: \.showInitial, to: initialCheck)
-        try? await loadUserData()
-    }
-    // 인앱결제 이루어졌을때 count가 어떻게 될지 모르겠어서 일단 남겨놓은.
-    func purchaseUserInitialCheck(user: User) async {
-        var initialCheck = user.showInitial
-        initialCheck += 10
-        try? await FirebaseManager.shared.update(data: user, value: \.showInitial, to: initialCheck)
-        
-        Task {
-            try await loadUserData()
-        }
     }
 }
 
