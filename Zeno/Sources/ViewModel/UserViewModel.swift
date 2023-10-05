825e989b64580bf18fa54b429f3fda16540daff1
yuhaeun-la <exok1and1m@gmail.com>
2023-10-05 16:05:35 +0900

    [Del] GroupSettingView삭제

--- Zeno/Sources/ViewModel/UserViewModel.swift
+++ Zeno/Sources/ViewModel/UserViewModel.swift
@@ -12,12 +12,11 @@ import FirebaseAuth
 import FirebaseFirestoreSwift
 
 class UserViewModel: ObservableObject {
-    private let firebaseManager = FirebaseManager.shared
     /// 파이어베이스 Auth의 User
     @Published var userSession: FirebaseAuth.User?
     /// 현재 로그인된 유저
     @Published var currentUser: User?
-    @Published var coolTime: Int = 8
+    private let coolTime: Int = 5
     
     init() {
         Task {
@@ -60,14 +59,20 @@ class UserViewModel: ObservableObject {
             print("🔴 회원가입 실패. 에러메세지: \(error.localizedDescription)")
         }
     }
+    /// 이메일 회원가입 정보 등록하기
+    @MainActor
+    func uploadUserData(user: User) async {
+        self.currentUser = user
+        try? await FirebaseManager.shared.create(data: user)
+    }
     /// 유저 데이터 가져오기
     @MainActor
     func loadUserData() async throws {
         self.userSession = Auth.auth().currentUser
         guard let currentUid = userSession?.uid else { return print("로그인된 유저 없음")}
-        // print("\(currentUid)")
+        print("\(currentUid)")
         self.currentUser = try await UserViewModel.fetchUser(withUid: currentUid)
-        // print("현재 로그인된 유저: \(currentUser ?? User.dummy[0])")
+        print("현재 로그인된 유저: \(currentUser ?? User.dummy[0])")
     }
     /// 로그아웃
     func logout() {
@@ -75,42 +80,6 @@ class UserViewModel: ObservableObject {
         self.userSession = nil
         self.currentUser = nil
     }
-    
-    /// 유저가 문제를 다 풀었을 경우, 다 푼 시간을 서버에 등록함
-    func updateZenoTimer() async {
-        do {
-            guard let currentUser = currentUser else { return }
-            let zenoStartTime = Date().timeIntervalSince1970
-            try await FirebaseManager.shared.update(data: currentUser, value: \.zenoEndAt, to: zenoStartTime + Double(coolTime))
-            try await loadUserData()
-            print("------------------------")
-            print("\(zenoStartTime)")
-            print("\(zenoStartTime + Double(coolTime))")
-            print("updateZenoTimer !! ")
-        } catch {
-            print("Error updating zeno timer: \(error)")
-        }
-    }
-    
-    /// 유저가 제노를 시작했는지, 안했는지 여부를 판단함 (서버가 맞을지 유저 디포츠가 맞을진 모르겟음)
-    func updateUserStartZeno(to: Bool) async {
-        do {
-            guard let currentUser = currentUser else { return }
-            try await FirebaseManager.shared.update(data: currentUser, value: \.startZeno, to: to)
-            try await loadUserData()
-            print("updateUserStartZeno ")
-        } catch {
-            print("Error updateStartZeno : \(error)")
-        }
-    }
-
-  
-	/// 로그아웃
-	func logout() {
-		try? Auth.auth().signOut()
-		self.userSession = nil
-		self.currentUser = nil
-	}
     
     /// 코인 사용 업데이트 함수
     func updateUserCoin(to: Int) async {
@@ -126,31 +95,47 @@ class UserViewModel: ObservableObject {
         self.currentUser?.showInitial += to
         try? await FirebaseManager.shared.update(data: currentUser, value: \.showInitial, to: initialCheck)
     }
-}
-
-     func comparingTime() -> Double {
-        let currentTime = Date().timeIntervalSince1970
-
-        if let currentUser = currentUser,
-           let zenoEndAt = currentUser.zenoEndAt {
-                return zenoEndAt - currentTime
-        } else {
-            return 0.0
-        }
-    }
     
-    /// 사용자한테 몇초 남았다고 초를 보여주는 함수
-    // MARK: 이 함수가 자원 갉아먹고 있음 
-    func comparingTime() -> Double {
-        let currentTime = Date().timeIntervalSince1970
+    /// 유저가 문제를 다 풀었을 경우, 다 푼 시간을 서버에 등록함
+     func updateZenoTimer() async {
+         do {
+             guard let currentUser = currentUser else { return }
+             let zenoStartTime = Date().timeIntervalSince1970
+             try await FirebaseManager.shared.update(data: currentUser, value: \.zenoEndAt, to: zenoStartTime + Double(coolTime))
+             try await loadUserData()
+             print("------------------------")
+             print("\(zenoStartTime)")
+             print("\(zenoStartTime + Double(coolTime))")
+             print("updateZenoTimer !! ")
+         } catch {
+             print("Error updating zeno timer: \(error)")
+         }
+     }
+     
+     /// 유저가 제노를 시작했는지, 안했는지 여부를 판단함 (서버가 맞을지 유저 디포츠가 맞을진 모르겟음)
+     func updateUserStartZeno(to: Bool) async {
+         do {
+             guard let currentUser = currentUser else { return }
+             try await FirebaseManager.shared.update(data: currentUser, value: \.startZeno, to: to)
+             try await loadUserData()
+             print("updateUserStartZeno ")
+         } catch {
+             print("Error updateStartZeno : \(error)")
+         }
+     }
 
-        if let currentUser = currentUser,
-           let zenoEndAt = currentUser.zenoEndAt {
-                return zenoEndAt - currentTime
-        } else {
-            return 0.0
-        }
-    }
+     /// 사용자한테 몇초 남았다고 초를 보여주는 함수
+     // MARK: 이 함수가 자원 갉아먹고 있음
+     func comparingTime() -> Double {
+         let currentTime = Date().timeIntervalSince1970
+         
+         if let currentUser = currentUser,
+            let zenoEndAt = currentUser.zenoEndAt {
+             return zenoEndAt - currentTime
+         } else {
+             return 0.0
+         }
+     }
 }
 
 /// static 메서드 모아놓은 extension
