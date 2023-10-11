a6973c6ebaddd0d88c3ec29fc5fe649bea8a3c21
Park Seo Yeon <syss220211@gmail.com>
2023-10-11 11:35:17 +0900

    fix: 타이머 중복 오류 수정
    onappear에 타이머 초기화 추가로 중복으로 계산되는 버그 수정완료

--- Zeno/Sources/View/MyPage/MegaphoneView.swift
+++ Zeno/Sources/View/MyPage/MegaphoneView.swift
@@ -9,25 +9,28 @@
 import SwiftUI
 
 struct MegaphoneView: View {
+    @EnvironmentObject private var userViewModel: UserViewModel
+    
     var body: some View {
-        //        GeometryReader{ geometry in
         HStack {
             Image(systemName: "speaker.wave.2.fill")
                 .font(.system(size: 30))
                 .foregroundColor(.red)
                 .fontWeight(.bold)
-            Text("확성기가 4회 남았어요.")
+            Text("확성기가 \(userViewModel.currentUser?.megaphone ?? 0)회 남았어요.")
                 .foregroundColor(.white)
                 .font(.system(size: 20))
         }
-        .frame(width: UIScreen.main.bounds.width, height: 60)
+//        .frame(width: UIScreen.main.bounds.width, height: 60)
+        .frame(maxWidth: .infinity)
+        .frame(height: 80)
         .background(.black)
-        //        }
     }
 }
 
 struct MegaphoneView_Previews: PreviewProvider {
     static var previews: some View {
         MegaphoneView()
+            .environmentObject(UserViewModel())
     }
 }
--- Zeno/Sources/View/MyPage/MyPageMain.swift
+++ Zeno/Sources/View/MyPage/MyPageMain.swift
@@ -7,21 +7,33 @@
 //
 
 import SwiftUI
+import Kingfisher
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
 
 struct MyPageMain: View {
     @EnvironmentObject private var userViewModel: UserViewModel
+    
+//    private var mypageViewModel = MypageViewModel()
     @State private var isShowingSettingView = false
     @State private var isShowingZenoCoin = true // 첫 번째 뷰부터 시작
+    @State private var timer: Timer?
     
     let coinView = CoinView()
     let megaphoneView = MegaphoneView()
     
     private func startTimer() {
-        Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
             withAnimation {
                 isShowingZenoCoin.toggle()
             }
         }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
     }
     
     var body: some View {
@@ -29,22 +41,27 @@ struct MyPageMain: View {
             ScrollView {
                 VStack(spacing: 0) {
                     HStack {
-                        AsyncImage(url: URL(string: (userViewModel.currentUser?.imageURL) ?? "https://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg")) { image in
-                            image
+                        // 유저 프로필 이미지 설정
+                        if let imageURLString = userViewModel.currentUser?.imageURL,
+                           let imageURL = URL(string: imageURLString) {
+                            KFImage(imageURL)
+                                .placeholder {
+                                    ProgressView()
+                                }
                                 .resizable()
                                 .scaledToFill()
                                 .frame(width: 100, height: 100)
                                 .clipShape(RoundedRectangle(cornerRadius: 30))
                                 .padding()
-                        } placeholder: {
-                            ProgressView()
+                        } else {
+                            KFImage(URL(string: "https://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg"))
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 100, height: 100)
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                                .padding()
                         }
-//                        Image("\(userViewModel.currentUser?.imageURL)")
-//                            .resizable()
-//                            .scaledToFill()
-//                            .frame(width: 100, height: 100)
-//                            .clipShape(RoundedRectangle(cornerRadius: 30))
-//                            .padding()
+
                         VStack(alignment: .leading) {
                             HStack {
                                 Text(userViewModel.currentUser?.name ?? "이름")
@@ -57,22 +74,22 @@ struct MyPageMain: View {
                                     Image(systemName: "greaterthan")
                                 }
                             }
-                            Text("저는 사과러버에요.")
+                            Text(userViewModel.currentUser?.description ?? " ")
                         }
                         Spacer()
-                        
-                        //                        Spacer()
                     }
                     .foregroundColor(.black)
-                    //					.padding(.bottom, 30)
-                    
+//
                     UserMoneyView()
+                        .environmentObject(userViewModel)
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 0) {
                             if isShowingZenoCoin {
                                 coinView
+                                    .environmentObject(userViewModel)
                             } else {
                                 megaphoneView
+                                    .environmentObject(userViewModel)
                             }
                         }
                         .frame(width: UIScreen.main.bounds.width, height: 60)
@@ -80,9 +97,16 @@ struct MyPageMain: View {
                     .background(Color.black)
                     .onAppear {
                         startTimer()
+                        
+                        print("👁️ 유저 커뮤니티 id 정보\(String(describing: userViewModel.currentUser?.commInfoList))")
+                    }
+                    .onDisappear {
+                        print("⏰ 타이머 끝")
+                        stopTimer()
                     }
                     GroupSelectView()
                         .foregroundColor(.black)
+                        .environmentObject(userViewModel)
                 }
             }
             .foregroundColor(.white)
@@ -106,7 +130,50 @@ struct MyPageMain_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             MyPageMain()
-                .environmentObject(UserViewModel())
+                .environmentObject(UserViewModel(currentUser: .fakeCurrentUser))
+        }
+    }
+}
+
+extension MyPageMain {
+    /*
+     @MainActor
+     func addRequestComm(comm: Community) async throws {
+         guard var currentUser else { return }
+         let requestComm = currentUser.requestComm + [comm.id]
+         try await firebaseManager.update(data: currentUser.self,
+                                          value: \.requestComm,
+                                          to: requestComm)
+         self.currentUser?.requestComm = requestComm
+     }
+     */
+    func findGroup() {
+        guard let currentUser = self.userViewModel.currentUser else { return }
+        let test = currentUser.commInfoList
+    }
+    
+    
+    func userGroupList() {
+        let db = Firestore.firestore()
+        
+        if let currentUser = userViewModel.currentUser?.commInfoList {
+            print("\(currentUser[0].id)")
+            db.collection("Community").document(currentUser[0].id).getDocument { document, error in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    
+                    if let fieldValue = data?["name"] as? String {
+                        print("fieldValue : \(fieldValue)")
+                    } else {
+                        print("실패")
+                    }
+                } else {
+                    print("firebase document 존재 오류")
+                }
+            }
+//            for group in currentUser {
+//
+//            }
         }
     }
 }
