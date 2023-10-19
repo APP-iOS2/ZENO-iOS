1acf8224dcb40b7df0633325a1f9744e34e971a3
Park Seo Yeon <syss220211@gmail.com>
2023-10-19 10:03:24 +0900

    [fix] progressView trailing 배치 에러 수정

--- Zeno/Sources/View/MyPage/MypageFirendListView.swift
+++ Zeno/Sources/View/MyPage/MypageFirendListView.swift
@@ -19,7 +19,7 @@ struct MypageFriendListView: View {
     @State private var isFetchingData = false
     
     var body: some View {
-        VStack(alignment: .trailing, spacing: 0) {
+        VStack {
             Picker("그룹선택", selection: $selectedGroupID) {
                 Text("전체").tag("all")
                     .font(ZenoFontFamily.NanumSquareNeoOTF.bold.swiftUIFont(size: 14))
@@ -29,6 +29,7 @@ struct MypageFriendListView: View {
                         .tag(mypageViewModel.commArray[group].id)
                 }
             }
+            .frame(maxWidth: .infinity, alignment: .trailing)
             .tint(.primary)
             .onChange(of: selectedGroupID) { newValue in
                 isFetchingData = true
@@ -49,87 +50,86 @@ struct MypageFriendListView: View {
                     }
                 }
             }
-            
-            VStack(alignment: .leading) {
-                if isFetchingData {
-                    ProgressView()
+            if isFetchingData {
+                ProgressView()
+                    .padding()
+            } else {
+                if mypageViewModel.friendInfo.isEmpty {
+                    VStack {
+                        LottieView(lottieFile: "friendNone")
+                            .frame(width: .screenWidth * 0.5, height: .screenHeight * 0.2)
+                            .opacity(0.7)
+                        Text("아직 추가된 친구가 없어요!")
+                            .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 15))
+                        Text("그룹에서 친구를 추가해보세요.")
+                            .font(ZenoFontFamily.NanumSquareNeoOTF.light.swiftUIFont(size: 13))
+                    }
+                    .foregroundColor(.primary)
+                    .frame(maxWidth: .infinity)
                 } else {
-                    if mypageViewModel.friendInfo.isEmpty {
-                        VStack {
-                            LottieView(lottieFile: "friendNone")
-                                .frame(width: .screenWidth * 0.5, height: .screenHeight * 0.2)
-                                .opacity(0.7)
-                            Text("아직 추가된 친구가 없어요!")
-                                .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 15))
-                            Text("그룹에서 친구를 추가해보세요.")
-                                .font(ZenoFontFamily.NanumSquareNeoOTF.light.swiftUIFont(size: 13))
-                        }
+                    Text("친구 \(mypageViewModel.friendInfo.count)명")
+                        .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 14.5))
                         .foregroundColor(.primary)
-                        .frame(maxWidth: .infinity)
-                    } else {
-                        Text("친구 \(mypageViewModel.friendInfo.count)명")
-                            .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 14.5))
-                            .foregroundColor(.primary)
-                        ForEach(mypageViewModel.friendInfo, id: \.self) { friend in
-                            if let friendInfo = friend {
-                                HStack {
-                                    if let imageURLString = friendInfo.imageURL,
-                                       let imageURL = URL(string: imageURLString) {
-                                        KFImage(imageURL)
-                                            .placeholder {
-                                                ProgressView()
-                                            }
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(width: 70, height: 70)
-                                            .clipShape(Circle())
-                                            .padding(8)
-                                    } else {
-                                        ZenoKFImageView(User(name: "", gender: friendInfo.gender, kakaoToken: "", coin: 0, megaphone: 0, showInitial: 0, requestComm: []), ratio: .fit, isRandom: false)
-                                            .frame(width: 70, height: 70)
-                                            .clipShape(Circle())
-                                            .padding(8)
-                                    }
-                                    VStack(alignment: .leading, spacing: 10) {
-                                        Text(friendInfo.name)
-                                            .font(ZenoFontFamily.NanumSquareNeoOTF.bold.swiftUIFont(size: 15))
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                        Text(friendInfo.description)
-                                            .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 13))
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                    }
-                                    .foregroundColor(.primary)
-                                    .frame(maxWidth: .infinity)
-                                    Spacer()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 20)
+                    ForEach(mypageViewModel.friendInfo, id: \.self) { friend in
+                        if let friendInfo = friend {
+                            HStack {
+                                if let imageURLString = friendInfo.imageURL,
+                                   let imageURL = URL(string: imageURLString) {
+                                    KFImage(imageURL)
+                                        .placeholder {
+                                            ProgressView()
+                                        }
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 70, height: 70)
+                                        .clipShape(Circle())
+                                        .padding(8)
+                                } else {
+                                    ZenoKFImageView(User(name: "", gender: friendInfo.gender, kakaoToken: "", coin: 0, megaphone: 0, showInitial: 0, requestComm: []), ratio: .fit, isRandom: false)
+                                        .frame(width: 70, height: 70)
+                                        .clipShape(Circle())
+                                        .padding(8)
                                 }
-                                Divider()
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text(friendInfo.name)
+                                        .font(ZenoFontFamily.NanumSquareNeoOTF.bold.swiftUIFont(size: 15))
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    Text(friendInfo.description)
+                                        .font(ZenoFontFamily.NanumSquareNeoOTF.regular.swiftUIFont(size: 13))
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity)
+                                Spacer()
                             }
+                            Divider()
                         }
                     }
+                    .padding(.horizontal, 20)
                 }
             }
-            .padding(.horizontal, 20)
-            .task {
-                isFetchingData = true
-                /// 유저의 gorupList, groupIDList, userInfo, friendIDList 가져오기
-                await mypageViewModel.getUserInfo()
-                guard let groupFriendID = mypageViewModel.friendIDList else { return }
-                mypageViewModel.groupFirendList = groupFriendID.removeDuplicates()
-                mypageViewModel.allMyPageFriendInfo = []
-                await mypageViewModel.getCommunityInfo() // 유저가 속한 전체 그룹의 이가져오는 함수 실행
-                mypageViewModel.friendInfo = []
-                mypageViewModel.allMyPageFriendInfo = []
-                if selectedGroupID == "all" {
-                    await mypageViewModel.getAllFriends()
-                    mypageViewModel.friendInfo = mypageViewModel.allMyPageFriendInfo.removeDuplicates()
-                    isFetchingData = false
-                } else {
-                    await mypageViewModel.getAllFriends()
-                    await mypageViewModel.returnFriendInfo(selectedGroupID: selectedGroupID)
-                    isFetchingData = false
-                }
+        }
+        .task {
+            isFetchingData = true
+            /// 유저의 gorupList, groupIDList, userInfo, friendIDList 가져오기
+            await mypageViewModel.getUserInfo()
+            guard let groupFriendID = mypageViewModel.friendIDList else { return }
+            mypageViewModel.groupFirendList = groupFriendID.removeDuplicates()
+            mypageViewModel.allMyPageFriendInfo = []
+            await mypageViewModel.getCommunityInfo() // 유저가 속한 전체 그룹의 이가져오는 함수 실행
+            mypageViewModel.friendInfo = []
+            mypageViewModel.allMyPageFriendInfo = []
+            if selectedGroupID == "all" {
+                await mypageViewModel.getAllFriends()
+                mypageViewModel.friendInfo = mypageViewModel.allMyPageFriendInfo.removeDuplicates()
+                isFetchingData = false
+            } else {
+                await mypageViewModel.getAllFriends()
+                await mypageViewModel.returnFriendInfo(selectedGroupID: selectedGroupID)
+                isFetchingData = false
             }
-            Spacer()
         }
     }
 }
